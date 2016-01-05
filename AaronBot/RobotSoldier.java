72fe3373e22b81d3f7d3f1ef66846000903ee52d
Aaron Wojnowski <aaronwojnowski@gmail.com>
2016-01-05 16:29:49 -0600

    Add AaronBot.

---
+++ AaronBot/RobotSoldier.java
@@ -0,0 +1,19 @@
+package AaronBot;
+
+import battlecode.common.*;
+
+public class RobotSoldier implements Robot {
+
+    public void run(final RobotController robotController) throws GameActionException {
+
+        while (true) {
+
+
+
+            Clock.yield();
+
+        }
+
+    }
+
+}
