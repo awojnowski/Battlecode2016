1442fd0ae14b282bcea05c3cbd52437c5ffd45a7
Aaron Wojnowski <aaronwojnowski@gmail.com>
2016-01-11 14:19:27 -0600

    Add new classes

---
+++ AaronBot2/Map/MapInfoModule.java
@@ -0,0 +1,12 @@
+package AaronBot2.Map;
+
+public class MapInfoModule {
+
+    public static int UnknownValue = Integer.MAX_VALUE;
+
+    public int northBoundaryValue = MapInfoModule.UnknownValue;
+    public int eastBoundaryValue = MapInfoModule.UnknownValue;
+    public int southBoundaryValue = MapInfoModule.UnknownValue;
+    public int westBoundaryValue = MapInfoModule.UnknownValue;
+
+}
